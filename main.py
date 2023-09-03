7fd5babd095a1e460b25b31b0c3229eeb837519d
Averium <solyomvaari@gmail.com>
2023-09-03 10:41:49 +0200

    Initial commit

--- main.py
+++ main.py
@@ -12,10 +12,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# TODO:
-#  Update theme handling to a more dynamic approach and add a theme switch or dropdown
-#  Sometimes there is a wierd flickering in actuated signals (possibly comes from C side)
-#  Something is wrong with the force vector X and Y coordinate length ratio
-#  Rework source code of RequestScreen and AllocationScreen widgets (they are too complicated)
